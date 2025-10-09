62bf1bcba9756e493d1dccc399827c4e99ea9a28
ZeroNewLife <workzero02@gmail.com>
2025-10-10 00:59:31 +0600

    One commit

---
+++ src/StableCoin.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title StableCoim
+ * @author Zero Web3
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ */
+contract StableCoin is ERC20Burnable, Ownable {
+    error YourBalanceLessZero();
+    error BurnAmountExedesBalance();
+    error NotZeroAddress();
+    error MastBeMoreThanZero();
+
+    constructor() ERC20("ZERO", "Z") Ownable(0xD843CBe0bdeE3E884Fd32cE4942219830D5944DA) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert YourBalanceLessZero();
+        }
+        if (balance < _amount) {
+            revert BurnAmountExedesBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert MastBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
---
+++ src/ZeroEngine.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+/**
+ * @title StableCoim
+ * @author Zero Web3
+ */
+import {StableCoin} from "./StableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract ZeroEngine is ReentrancyGuard{
+
+
+    error TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+    error NeedsMoreThanZero();
+    error TokenNotAllowed(address token);
+    error NotAllowedToken();
+    error TransferFailed();
+    error BreaksHealthFactor(uint256 healthFactorValue);
+    error MintFailed();
+    error HealthFactorOk();
+    error HealthFactorNotImproved();
+
+    StableCoin private immutable i_zero;
+    mapping(address token => address priceFeed) private s_priceFeeds;
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert NotAllowedToken();
+        }
+        _;
+    }
+
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address zeroAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+        }
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_zero = StableCoin(zeroAddress);
+    }
+
+    function depositCollateralAndMintZero() external {}
+
+    function depositCollateral(address addressColateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(addressColateralAddress)
+        nonReentrant
+    {
+        
+    }
+
+    function redeemCollateralForZero() external {}
+
+    function redeemCollateral() external {}
+
+    function mintZero() external {}
+
+    function burnZero() external {}
+
+    function liquidatte() external {}
+
+    function getHealthFactor() external {}
+}
